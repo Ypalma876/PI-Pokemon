4ee653953ec06eb1aee29f769ae76d43f1ae24ef
Yohana <palmayohanaly@gmail.com>
2022-06-04 11:22:16 -0400

    get types done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const Pokemon = require('./pokemons.js');
-//const Type = '';
+const Type = require('./types.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -10,8 +10,8 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use(' /pokemons', Pokemon);
-//router.use('/types', Type)
+router.use('/pokemons', Pokemon);
+router.use('/types', Type)
 
 
 module.exports = router;
--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -1,3 +1,22 @@
-const { Router } = require('express');
-const { Type } = require('../db.js');
+const { Type } = require("../db.js");
 
+/////////////////////GET TYPES ///////////////////////////////////
+
+const getTypes = async (req, res) => {
+  const typesDB = await Type.findAll();
+
+    if(typesDB.length === 0) {
+        try {
+            const typesApi = await fetch('https://pokeapi.co/api/v2/type')
+            const types = await typesApi.json()
+            for(let i=0; i<types.data.results.length; i++){
+                await Type.create({name: types.data.results[i].name});
+            }
+         } catch(error) {
+           return res.status(404).send('Ha ocurrido un error')
+         }
+        } else {
+            return res.status(200).json(typesDB);
+        }
+}
+module.exports = getTypes;
